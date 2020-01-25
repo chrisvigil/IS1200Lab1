6de092ec8d8bd8ab894022d89f8e87630f1bb21f
Chris Vigil <chrisxvigil@gmail.com>
2020-01-25 21:20:40 +0100

    Fixed comments

--- analyze.asm
+++ analyze.asm
@@ -11,9 +11,9 @@ loop:
 	li	$v0,11		# syscall with v0 = 11 will print out
 	syscall			# one byte from a0 to the Run I/O window
 
-	addi	$s0,$s0,3	# Chnaged constent from 1 to 3, this causes $s0 to advanvce by 3 each loop
+	addi	$s0,$s0,3	# Changed constent from 1 to 3, this causes $s0 to advance by 3 each loop
 	
-	li	$t0,0x5d	# Needed to be modifed as caning the constent above causes $s0 to never be equal to the pervious value.
+	li	$t0,0x5d	# Needed to be modifed as changing the constent above caused $s0 to never be equal to the pervious value.
 	bne	$s0,$t0,loop
 	nop			# delay slot filler (just in case)
 
