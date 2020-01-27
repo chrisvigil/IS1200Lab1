94d4d3951308be01e8768e120e1f763fdd581428
Chris Vigil <chrisxvigil@gmail.com>
2020-01-27 17:24:53 +0100

    adjusted code for delayed branching

--- timetemplate.asm
+++ timetemplate.asm
@@ -79,12 +79,14 @@ hexasc:
 	beq 	$t0,$zero,else  # branches to else if greater then nine
 	addi	$v0,$a0,0x30	# adds 0x30 to get correct hex value for 0 to 9
 	j	L1
+	nop
 else:
 	addi	$v0,$a0,0x37	# adds 0x37 to get correct hex value for A to F
 	
 L1:
 	andi	$v0,$v0,0x7f	# sets bit 8 to MSB to 0 to ensure result is an ASCII value
 	jr	$ra
+	nop
 	
 delay:
 	ble	$a0,$0,delayd 	# while a0 > 0
@@ -96,6 +98,7 @@ dL:	ble	$t0,$t1,delay	# while t1 < t0
 	addi	$t1,$t1,1	# t1++	
 	nop
 	j 	dL
+	nop
 delayd:	jr $ra
 	nop
 
@@ -109,11 +112,13 @@ time2string:
 	and	$a0,$a1,0xf000	# mask
 	srl 	$a0,$a0,12	# shift right for hexasc
 	jal	hexasc		# convert ascii char
+	nop
 	sb	$v0,0($s0)	# store ascii char code to string
 	
 	and	$a0,$a1,0xf00	# mask
 	srl 	$a0,$a0,8	# shift right for hexasc
 	jal	hexasc		# convert ascii char
+	nop
 	sb	$v0,1($s0)	# store ascii char code to string
 	
 	addi	$v0,$0,0x3a	# set v0 to :
@@ -122,10 +127,12 @@ time2string:
 	and	$a0,$a1,0xf0	# mask
 	srl 	$a0,$a0,4	# shift right for hexasc
 	jal	hexasc		# convert ascii char
+	nop
 	sb	$v0,3($s0)	# store ascii char code to string
 	
 	and	$a0,$a1,0xf	# mask
 	jal	hexasc		# convert ascii char
+	nop
 	sb	$v0,4($s0)	# store ascii char code to string
 	
 	sb	$0,5($s0)	# append null byte
@@ -135,6 +142,7 @@ time2string:
 	POP	($s0)
 	POP	($ra)
 	jr	$ra
+	nop
 	
 	
 	
