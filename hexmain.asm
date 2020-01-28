8842cabfaddf4d350da9f8e01f62dbc0369bf92b
Chris Vigil <chrisxvigil@gmail.com>
2020-01-28 14:29:13 +0100

    changed hexac to mask initially

--- hexmain.asm
+++ hexmain.asm
@@ -4,7 +4,7 @@
 
 	.text
 main:
-	li	$a0,10		# change this to test different values
+	li	$a0,15		# change this to test different values
 
 	jal	hexasc		# call hexasc
 	nop			# delay slot filler (just in case)	
@@ -19,6 +19,7 @@ stop:	j	stop		# stop after one run
 
 .global hexasc
 hexasc:
+	andi	$a0,$a0,0xf	# sets all bits except the first 4 bits to 0
 	slti	$t0,$a0,10 	# sets t0 to 1 if less then 10, else 0
 	beq 	$t0,$zero,else  # branches to else if greater then nine
 	addi	$v0,$a0,0x30	# adds 0x30 to get correct hex value for 0 to 9
@@ -27,5 +28,4 @@ else:
 	addi	$v0,$a0,0x37	# adds 0x37 to get correct hex value for A to F
 	
 L1:
-	andi	$v0,$v0,0x7f	# sets bit 8 to MSB to 0 to ensure result is an ASCII value
 	jr	$ra
